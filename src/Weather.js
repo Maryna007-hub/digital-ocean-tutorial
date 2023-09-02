44bc150ac95bea093590d76844166b885ef24064
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-02 21:09:29 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -27,7 +27,7 @@ export default function Weather(props) {
                     <input type="search" placeholder="Enter a city..." className="form-control"/>
                 </div>
                 <div className="col-3">
-                    <input type="submit" value="Search" className="btn btn-primary"/>
+                    <input type="submit" value="Search" className="btn btn-primary w-100"/>
                 </div>
             </div>
             </form>
