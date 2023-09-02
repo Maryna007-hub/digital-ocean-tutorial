d347428ca8617142f7555d86f75dbff2164d7488
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-02 16:27:37 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
        
         <footer>
           This project was coded by 
-          <a href='/'>Maryna Holinka</a>
+          <a href='/'>Maryna Holinka </a>
           and is 
         <a href='https://github.com/Maryna007-hub/digital-ocean-tutorial'
           target="_blank"
