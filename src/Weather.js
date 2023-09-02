8daad0bbcc70bc26a2588b6a3533cba6681d564d
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-02 18:31:22 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,7 @@ export default function Weather() {
     
 return (
 <div className="Weather"> 
+<form>
 <div className="row">
     <div className="col-9">
         <input type="search" placeholder="Enter a city..." className="form-control"/>
@@ -12,6 +13,7 @@ return (
         <input type="submit" value="Search" className="btn btn-primary"/>
     </div>
 </div>
+</form>
 <h1> Lisbon</h1>
 <ul>
     <li>
