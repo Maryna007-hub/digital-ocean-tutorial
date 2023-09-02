8fd58837b82f4efebc1496ac53dc6e029c40fdfd
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-02 18:07:01 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./Weather.css";
 export default function Weather() {
     
-return (<div className="Weather"> 
+return (
+<div className="Weather"> 
+<div className="row">
+    <div className="col-9">
+        <input type="search" placeholder="Enter a city" className="form-controll"/>
+    </div>
+    <div className="col-3">
+        <input type="submit" value="Search" className="btn btn-primary"/>
+    </div>
+</div>
 <h1> Lisbon</h1>
 <ul>
     <li>
