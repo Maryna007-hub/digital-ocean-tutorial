b70b58fb6bc91e8518b767167b9c49f8402f3d94
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-02 22:45:49 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -27,7 +27,8 @@ export default function Weather(props) {
             <div className="row">
                 <div className="col-9">
                     <input type="search" placeholder="Enter a city..." 
-                    className="form-control"/>
+                    className="form-control"
+                    autoFocus="on"/>
                 </div>
                 <div className="col-3">
                     <input type="submit" value="Search" className="btn btn-primary w-100"/>
