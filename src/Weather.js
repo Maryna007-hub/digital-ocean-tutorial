fd0eba6a95cae5de407a4040b9537ac18b696800
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-02 18:12:25 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,7 @@ return (
 <div className="Weather"> 
 <div className="row">
     <div className="col-9">
-        <input type="search" placeholder="Enter a city" className="form-controll"/>
+        <input type="search" placeholder="Enter a city..." className="form-controll"/>
     </div>
     <div className="col-3">
         <input type="submit" value="Search" className="btn btn-primary"/>
