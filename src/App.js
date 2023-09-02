eb07c546a25033bef340eaf883c94919dfa5ccb4
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-02 23:38:17 +0100

    upd

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ export default function App() {
        <Weather defaultCity="Oslo"/>
         <footer>
           This project was coded by 
-          <a href='/'>Maryna Holinka </a>
-          and is <a href='https://github.com/Maryna007-hub/digital-ocean-tutorial'
+           <a href='/'>Maryna Holinka </a>
+           and is <a href='https://github.com/Maryna007-hub/digital-ocean-tutorial'
           target="_blank"
           rel="noopener noreferrer">
      open-soursed on GitHub
