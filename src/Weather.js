59a757b50eb1beebe0dd422586e6da3470ffc897
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-02 22:33:31 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -34,8 +34,7 @@ export default function Weather(props) {
                 </div>
             </div>
             </form>
-            <WeatherInfo/>
-           
+            <WeatherInfo data={weatherData}/>
             </div>
              );  
     } else {
