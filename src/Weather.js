bf356c1ec90762b72c12ecd2f40e8fa9295e2b7f
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-02 17:28:00 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,15 @@ import "./Weather.css";
 export default function Weather() {
     
 return (<div className="Weather"> 
-hello
+<h1> Lisbon</h1>
+<ul>
+    <li>
+        Monday 20:00
+    </li>
+    <li>
+        Clear
+    </li>
+</ul>
 </div>
     );
 }
