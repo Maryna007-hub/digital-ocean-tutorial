74986a6639d116548410abcf6d427b0cacfeff41
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-03 12:36:11 +0100

    upd

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactAnimatedWeather from 'react-animated-weather';
 
 export default function WeatherIcon(props) {
-    console.log(props.code);
     const codeMapping = {
         "01d": "CLEAR_DAY",
         "01n": "CLEAR_NIGHT",
@@ -27,7 +26,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
     icon={codeMapping[props.code]}
     color='darkblue'
-    size={64}
+    size={props.size}
     animate={true}
   />
  );
