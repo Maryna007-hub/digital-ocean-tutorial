d3c703e107a94ba3005f9081ba2ffbf2aa1800b6
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-03 13:50:42 +0100

    upd

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,6 +19,7 @@ export default function WeatherInfo (props) {
                 <div className="clearfix">
                     <div className="float-left">
                       <WeatherIcon code={props.data.icon}/>
+                      <WeahterForecast />
                     </div>
                 
                 <div className="float-left">
