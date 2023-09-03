3a665cfeccfda261e55ae6444f6031415c8b642a
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-03 12:56:11 +0100

    sum

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import  React  from "react";
 import FormattedDate from "./FormattedDate";
 import  WeatherIcon  from "./WeatherIcon";
+import  WeatherTemp  from "./WeatherTemp";
 
 export default function WeatherInfo (props) {
     return (
@@ -21,9 +22,7 @@ export default function WeatherInfo (props) {
                     </div>
                 
                 <div className="float-left">
-                    <span className="temperature">{Math.round(props.data.temperature)}</span>
-                    <span className="unit">°C  °F</span>
-                
+                <WeatherTemperature celsius={props.data.temperature}/>
                 </div>
              </div>
              </div>
--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,1 +1,41 @@
 import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+    const [unit, setUnit] = useState("celsius");
+   
+       function showFaherheit(event) {
+       event.preventDefault();
+       setUnit("fahrenheit");
+     }
+   
+     function showCelsius(event) {
+       event.preventDefault()
+       setUnit("celsius");
+     }
+      if (unit === "celsius") {
+        return (
+         <div className="WeatherTemperature">
+            <span className="temperature">{Math.round(props.celsius)}</span>
+             <span className="unit">
+                  °C |{" "}
+                  <a href="/" onClick={showFaherheit}>
+                  °F
+                  </a>
+               </span>
+         </div>
+          );
+       } else {
+           let fahrenhiet = (props.celsius * 9) / 5 + 32;
+           return (
+               <div className="WeatherTemperature">
+                  <span className="temperature">{Math.round(fahrenhiet)}</span>
+                   <span className="unit">
+                        <a href="/" onClick={showCelsius}>
+                        °C 
+                        </a>{" "}
+                       | °F
+                     </span>
+               </div>
+                );
+       }
+   }  
