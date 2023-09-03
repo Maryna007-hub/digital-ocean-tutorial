466e36634c57c600a55557c6b270cf0fe8bea690
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-03 17:53:40 +0100

    upd

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
          wind: response.data.wind.speed,
          icon: response.data.weather[0].icon,
          date: new Date(response.data.dt * 1000),
-         coord: response.data.coord,
+         coordinates: response.data.coord,
          ready: true,
          })
     }
@@ -53,7 +53,7 @@ function handleCityChange(event) {
             </div>
             </form>
             <WeatherInfo data={weatherData}/>
-            <WeatherForecast />
+            <WeatherForecast coordinates={weatherData.coordinates}/>
             </div>
              );  
     } else {
