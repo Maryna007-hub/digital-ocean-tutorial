209563e73545c3013911bcfb020ed2936791f1cc
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-03 18:28:20 +0100

    upd

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,20 +5,12 @@ import axios from "axios";
 
 export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
-    let [forecast, setForecast] = useState(false);
+    let [forecast, setForecast] = useState(null);
     function handleResponse(response) {
-        console.log(response.data);
+      setForecast(response.data.daily);
     }
 if (loaded) {
-  let apiKey = "df04a6426eb8c9305ebb65c9deb52f35";
-let longitude = props.coordinates.lon;
-let latitude = props.coordinates.lat;
-let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?
-lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(handleResponse); 
-return null; 
-} else {
-  return (
+    return (
         <div className="WeatherForecast">
             <div className="row">
                 <div className="col-2">
@@ -32,6 +24,16 @@ return null;
             </div>
         </div>
     );
+
+
+} else {
+   let apiKey = "df04a6426eb8c9305ebb65c9deb52f35";
+let longitude = props.coordinates.lon;
+let latitude = props.coordinates.lat;
+let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?
+lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(handleResponse); 
+return null; 
 }  
 }
     
