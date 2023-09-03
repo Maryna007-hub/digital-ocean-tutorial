c6b4e50ac7e3010db5032bebbe9585a044b825b4
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-03 11:35:41 +0100

    sum

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,6 +15,7 @@ export default function WeatherInfo (props) {
              <div className="col-6">
 
                 <div className="clearfix">
+                    <WeatherIcon code={props.data.icon}/>
                 <img src={props.data.iconUrl}
                 alt={props.data.description} className="float-left"/>
                 
