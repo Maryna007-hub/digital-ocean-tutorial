fcfc5fba5b8c3330fe36ba61beada8edf8a830cc
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-03 21:07:14 +0100

    upd

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,15 +4,12 @@ import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
-   let [loaded, setLoaded] = useState(false);
-let [forecast, setForecast] = useState(null);
 
     function handleResponse(response) {
- setForecast(response.data.daily);
- setLoaded(true);
+ 
    }
    if (loaded) {
-    console.log(forecast);
+    console.log(setForecast);
      return (
         <div className="WeatherForecast">
             <div className="row">
