3932e72d56fee46a631cf66e333d61e5172ea05e
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-03 14:12:21 +0100

    upd

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,14 @@
 import  React  from "react";
+import WeatherIcon from "./WeatherIcon";
+
 export default function WeatherForecast(params) {
     return (
         <div className="WeatherForecast">
             <div className="row">
                 <div className="col">
-                    Mon Icon 19 10
+                    Mon 
+                    <WeatherIcon code="09n"/> 
+                    19 10
                 </div>
             </div>
         </div>
