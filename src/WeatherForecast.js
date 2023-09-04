63ee5cb394643af532979a26667b591d1d0efda5
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 18:34:36 +0100

    upd

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,38 +6,46 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
-  
-  function handleResponse(response) {
-    setForecast(response.data.list);
-    setLoaded(true);
-    // console.log(response.data);
-  }
-  // console.log(props);
-  if (loaded) {
-    // console.log(forecast);
-    return (
-        <div className="WeatherForecast">
-          <div className="row">
-            {forecast.map(function(dailyforecast, index) {
-              if (index < 5) {
-                return (
-                 <div className="col" key={index}>
-                <WeatherForecastDay data={dailyforecast}/>
-             </div>
-              )
-              }
-              
-            })}
-             </div>
-        </div>
-      ); 
-  } else {
-  let apiKey = "df04a6426eb8c9305ebb65c9deb52f35";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-return null;
-}
-}
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
+
+    return null;
+  }
+}
