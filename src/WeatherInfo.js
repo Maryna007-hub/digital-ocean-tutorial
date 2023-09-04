fa889c6fbd8afce7d6c292ff1b2be554931f45bf
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 19:51:58 +0100

    upd

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,21 +16,23 @@ export default function WeatherInfo (props) {
             <div className="row mt-3">
               <div className="col-6">
             
-              <div className="d-flex">
+              <div className="clearfix">
+                <div className="float-left">
                       <WeatherIcon code={props.data.icon} size={54} />
                     </div>
-                
-               <div>
-                <WeatherTemperature celsius={props.data.temperature}/>
+                <div className="float-left">
+                    <WeatherTemperature celsius={props.data.temperature}/>
                 </div>
-
+                </div>
                 </div>
                 </div>
              <div className="col-6">
+             <div className="clearfix">
              <ul>
                 <li>Humidity: {props.data.humidity}%</li>
                 <li>Wind: {Math.round(props.data.wind)} km/h</li>
             </ul>
+            </div>
              </div>
             </div> 
          );
