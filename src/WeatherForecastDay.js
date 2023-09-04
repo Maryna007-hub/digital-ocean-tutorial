4223629751a40559376758ed958e23ab493813dd
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 10:19:02 +0100

    upd

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -13,6 +13,13 @@ export default function WeatherforecastDay(props) {
     function day() {
         let date = new Date(props.data.dt *1000);
         let day = date.getDay();
+        let days = ["Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat"]
         return day;
     }
     return (
