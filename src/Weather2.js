6a0802887204a710de45627c347c92c3f5600cae
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 13:50:54 +0100

    sum

---
+++ src/Weather2.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import  WeatherInfo  from "./WeatherInfo";
+import  WeatherForecast  from "./WeatherForecast";
+import "./Weather.css";
+import  axios  from "axios";
+
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ready: false});
+    const [city, setCity] = useState(props.defaultCity);
+
+    function handleResponse(response) {
+
+       setWeatherData({
+         temperature: response.data.main.temp,
+         city: response.data.name,
+         humidity: response.data.main.humidity,
+         description: response.data.weather[0].description,
+         wind: response.data.wind.speed,
+         icon: response.data.weather[0].icon,
+         date: new Date(response.data.dt * 1000),
+         coordinates: response.data.coord,
+         ready: true,
+         })
+    }
+    function search() {
+        const apiKey = "df04a6426eb8c9305ebb65c9deb52f35";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=
+        ${city}&units=metric&appid=${apiKey}`;
+        axios.get(apiUrl).then(handleResponse);  
+    }
+function handleSubmit(event) {
+    event.preventDefault();
+    search();
+
+}
+function handleCityChange(event) {
+    setCity(event.target.value);
+}
+    if (weatherData.ready) {
+        return (
+            <div className="Weather"> 
+            <form onSubmit={handleSubmit}>
+            <div className="row">
+                <div className="col-9">
+                    <input type="search" placeholder="Enter a city..." 
+                    className="form-control"
+                    autoFocus="on"
+                    onChange={handleCityChange}/>
+                </div>
+                <div className="col-3">
+                    <input type="submit" value="Search" className="btn btn-primary w-100"/>
+                </div>
+            </div>
+            </form>
+            <WeatherInfo data={weatherData}/>
+            <WeatherForecast coordinates={weatherData.coordinates}/>
+            </div>
+             );  
+    } else {
+      search();
+        return "Loading...";
+        }
+}
---
+++ src/WeatherForecast2.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+  
+  function handleResponse(response) {
+    setForecast(response.data.list);
+    setLoaded(true);
+    // console.log(response.data);
+  }
+  // console.log(props);
+  if (loaded) {
+    // console.log(forecast);
+    return (
+        <div className="WeatherForecast">
+          <div className="row">
+             <div className="col">
+                <WeatherForecastDay data={forecast[0]}/>
+             </div>
+            
+          </div>
+        </div>
+      ); 
+  } else {
+  let apiKey = "df04a6426eb8c9305ebb65c9deb52f35";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+return null;
+}
+}
+
