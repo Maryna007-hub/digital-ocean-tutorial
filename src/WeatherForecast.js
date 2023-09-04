078b1b0de1012fab69e5ec7082441cd0812c992d
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 07:27:30 +0100

    upd

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -9,28 +9,31 @@ export default function WeatherForecast(props) {
      console.log(response.data);
   }
   // console.log(props);
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+          <div className="row">
+              <div className="col-2">
+                 <div className="WeatherForecast-day">Mon</div> 
+                   <WeatherIcon code="09n" size={36} /> 
+                    <div className="WeatherForecast-temperature">
+                      <span className="WeatherForecast-temperature-max">
+                       16°</span>
+                      <span className="WeatherForecast-temperature-min">
+                      5°</span>
+                    </div>
+                 </div>
+          </div>
+      </div>
+  ); 
+  } else {
   let apiKey = "df04a6426eb8c9305ebb65c9deb52f35";
  
   let longitude = props.coordinates.lon;
   let latitude = props.coordinates.lat;
   let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
-
-return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col-2">
-                   <div className="WeatherForecast-day">Mon</div> 
-                     <WeatherIcon code="09n" size={36} /> 
-                      <div className="WeatherForecast-temperature">
-                        <span className="WeatherForecast-temperature-max">
-                         16°</span>
-                        <span className="WeatherForecast-temperature-min">
-                        5°</span>
-                      </div>
-                   </div>
-            </div>
-        </div>
-    ); 
+return null;
+}
 }
 
