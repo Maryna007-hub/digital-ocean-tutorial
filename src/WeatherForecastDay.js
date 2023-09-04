f4a3a7fbe3ad62b219132e121d0e8f83c070842e
Maryna007-hub <marinagolinka007@gmail.com>
2023-09-04 11:15:53 +0100

    sum

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -23,7 +23,7 @@ export default function WeatherforecastDay(props) {
         return days[day];
     }
     return (
-        <div className="Test1">
+        <div>
              <div className="Weatherforecast-day">{day()}</div> 
                    <WeatherIcon code={props.data.weather[0].icon} size={36} /> 
                     <div className="Weatherforecast-temperature">
